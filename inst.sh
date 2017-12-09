057625002f572667ab8473bb5d3d77244a0ae55d
Chris <32462283+chrisseymour@users.noreply.github.com>
2017-12-09 02:07:45 -0500

    removed export statements

--- inst.sh
+++ inst.sh
@@ -13,11 +13,6 @@ sudo pip install cython
 sudo apt-get install default-jre 
 sudo apt-get install default-jdk
 
-###set paths
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export PATH=$PATH:$JAVA_HOME/bin
-export PATH=$PATH:/usr/lib/jvm/default-java/jre/lib/amd64/server
-
 ###get and install pyjnius
 git clone https://github.com/kivy/pyjnius.git
 cd pyjnius/
