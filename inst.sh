4b80dd220936f3bae6ed3ebf1f956d2f3a8f5408
Chris <32462283+chrisseymour@users.noreply.github.com>
2017-12-09 22:48:12 -0500

    buildozer and kivy

--- inst.sh
+++ inst.sh
@@ -6,7 +6,8 @@ sudo apt-get update
 sudo apt-get install python-setuptools python-dev
 sudo apt-get install build-essential ant
 sudo easy_install pip
-sudo pip install cython
+#sudo pip install  cython
+sudo pip install --upgrade cython==0.25.2 #needed for kivy (apt-get version)
 
 ###java requirements (JRE and JDK)
 sudo apt-get install default-jre 
@@ -18,3 +19,15 @@ git clone https://github.com/kivy/pyjnius.git
 cd pyjnius/
 sudo python setup.py install
 sudo make
+
+### add 32-bit files for android
+sudo dpkg --add-architecture i386
+sudo apt-get update
+sudo apt-get install libpangox-1.0-0:i386 libpangoxft-1.0-0:i386
+sudo apt-get install libsdl2-dev lib32z1
+
+### buildozer packager and kivy
+sudo pip install buildozer
+sudo pip -no-cache-dir install kivy
+buildozer init
+buildozer -v android debug
